5b4a9391891e24d32c1b8f256ee09501676966d9
Alexander Lychagov <consumedfuture@gmail.com>
2015-10-22 18:15:57 +0500

    добавил скрипт обработки клика

---
+++ itProject/Assets/Scripts/common/Click.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+	static int ClicksOnObjects = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	static public bool OnClick(GUITexture target)
+	{
+		if (PlayingPlatform.isPC())
+		{
+			if ((Input.GetKeyDown(KeyCode.Mouse0)) && (target.GetScreenRect().Contains(Input.mousePosition)))
+			{
+				ClicksOnObjects++;
+				return true;
+			}
+		}
+		if (PlayingPlatform.isMobile())
+		{
+			if ((Input.GetKeyDown(KeyCode.Mouse0)) && (target.GetScreenRect().Contains(Input.mousePosition)))
+			{
+				ClicksOnObjects++;
+				return true;
+			}
+		}
+		//Debug.Log (ClicksOnObjects);
+		return false;
+	}
+
+	static public bool OnClick(GUIText target)
+	{
+		if (PlayingPlatform.isPC())
+		{
+			if ((Input.GetKeyDown(KeyCode.Mouse0)) && (target.GetScreenRect().Contains(Input.mousePosition)))
+			{
+				ClicksOnObjects++;
+				return true;
+			}
+		}
+		if (PlayingPlatform.isMobile())
+		{
+			if ((Input.GetKeyDown(KeyCode.Mouse0)) && (target.GetScreenRect().Contains(Input.mousePosition)))
+			{
+				ClicksOnObjects++;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	static public bool OnPress(GUITexture target)
+	{
+		if (PlayingPlatform.isPC())
+		{
+			if ((Input.GetKey(KeyCode.Mouse0)) && (target.GetScreenRect().Contains(Input.mousePosition)))
+			{
+				ClicksOnObjects++;
+				return true;
+			}
+		}
+		if (PlayingPlatform.isMobile())
+		{
+			if ((Input.GetKey(KeyCode.Mouse0)) && (target.GetScreenRect().Contains(Input.mousePosition)))
+			{
+				ClicksOnObjects++;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	static public bool MouseOver(GUITexture target)
+	{
+		if (target.GetScreenRect().Contains(Input.mousePosition))
+			return true;
+		return false;
+	}
+
+	static public bool MouseOver(GUIText target)
+	{
+		if (target.GetScreenRect().Contains(Input.mousePosition))
+			return true;
+		return false;
+	}
+
+	static public bool RandomClick()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			if (ClicksOnObjects==0)
+				return true;
+		}
+		ClicksOnObjects = 0;
+		return false;
+	}
+}
