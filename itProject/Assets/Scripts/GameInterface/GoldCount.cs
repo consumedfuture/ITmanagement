aface7d3676bb2dcd765346c5745c9a2228f8900
Nikita Ivanov <nikitaivanov95@gmail.com>
2015-12-20 22:12:24 +0500

    Version 8
    Добавлен интерфейс игрового уровня

---
+++ itProject/Assets/Scripts/GameInterface/GoldCount.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoldCount : MonoBehaviour {
+
+	// Use this for initialization
+    string RusFormat;
+    string EngFormat;
+    int Gold;
+    Text txt;
+	void Start () {
+        RusFormat = "Количество золота: \n{0}";
+        EngFormat = "Gold count: \n{0}";
+        Gold = PlayerManagement.Gold;
+        txt = this.GetComponent<Text>();
+        if (Settings.Russian)
+            txt.text = string.Format(RusFormat, Gold);
+        else
+            txt.text = string.Format(EngFormat, Gold);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Gold != PlayerManagement.Gold)
+        {
+            Gold = PlayerManagement.Gold;
+            if (Settings.Russian)
+                txt.text = string.Format(RusFormat, Gold);
+            else
+                txt.text = string.Format(EngFormat, Gold);
+        }
+	}
+}
---
+++ itProject/Assets/Scripts/GameInterface/ManaCount.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ManaCount : MonoBehaviour {
+
+    string RusFormat;
+    string EngFormat;
+    int Mana;
+    Text txt;
+	// Use this for initialization
+	void Start () {
+        RusFormat = "Количество маны: \n{0} / {1}";
+        EngFormat = "Mana count: \n{0} / {1}";
+        Mana = PlayerManagement.Mana;
+        txt = this.GetComponent<Text>();
+        if (Settings.Russian)
+            txt.text = string.Format(RusFormat, Mana, PlayerManagement.MaxMana);
+        else
+            txt.text = string.Format(EngFormat, Mana, PlayerManagement.MaxMana);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mana != PlayerManagement.Mana)
+        {
+            Mana = PlayerManagement.Mana;
+            if (Settings.Russian)
+                txt.text = string.Format(RusFormat, Mana, PlayerManagement.MaxMana);
+            else
+                txt.text = string.Format(EngFormat, Mana, PlayerManagement.MaxMana);
+        }
+	}
+}
