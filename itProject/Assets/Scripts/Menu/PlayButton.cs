e0127d6f88efbcb7e1b7a4a44ec7ae9d00f30d2a
Sergey Ilchenko <digernaut@gmail.com>
2015-12-22 18:07:36 +0500

    меню

---
+++ itProject/Assets/Scripts/Menu/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour {
+
+    public int Level;
+    Button btn;
+	// Use this for initialization
+	void Start () {
+        btn = GetComponent<Button>();
+        if (Level > Settings.MaxLevel)
+            btn.interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void PlayGame(int level)
+    {
+        Field.CurrentLevel = level;
+        Field.Reset();
+        Application.LoadLevel("game");
+    }
+}
